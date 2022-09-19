a1e34a0ff55228eb33c8dd0a4afe558010174f8a
ajpguillermo <aguillermo@vertere-gs.com>
2022-09-19 14:35:50 +0800

    Add activity code

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+const firstName = "Arden Joseph";
+const lastName = "Guillermo";
+let age = '28';
+const hobbies = ["Playing games", "Reading manga", "Taking walks"];
+let workAddress = {houseNumber:"Block 1 Lot 8", street: "Buckeye Street, Richview Square", city: "Baguio City", state: "Benguet"};
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Age: " + age);
+console.log("Hobbies:");
+console.log(hobbies);
+console.log("Work Address:");
+console.log(workAddress);
+
+console.log("My full name is: " + firstName + " " + lastName);
+console.log("My current age is: " + age);
+console.log("My hobbies are:");
+console.log(hobbies);
+console.log("My work address is: ");
+console.log(workAddress);
